04fa01a58b25b95b38ea490c386e4889133880bb
Baptiste Rajaut <baptiste@rajaut.fr>
2017-07-06 14:04:51 +0200

    +[libi_formBuilder] : Way more customizable

--- libi_files/libi_FormBuilder.php
+++ libi_files/libi_FormBuilder.php
@@ -1,19 +1,24 @@
 <?php
 class FormBuilder{
 	private $form;
-	
+	private $begin='<table border="0">';
+	private $end='</table></form>';
+	private $beginEach='<tr><td>';
+
+	private $betweenEach='</td><td>';
+	private $endEach='</td></tr>';
 	public function __construct($args="",$page='',$type='POST'){
 	$this->form='<form '.$args.' method="'.$type.'" action="'.$page.'">
-		<table border="0">';
+		';
 		
 	}
-	public function addInput($name,$echo,$type='',$args=''){
-		$this->form.='<tr><td><label for="' . $name . '">' . $echo . '</label></td>'."<td><input type=\"$type\" name=\"$name\" id=\"$name\" $args /></td></tr>";
+	public function addInput($name,$echo,$type='text',$args=''){
+		$this->form.= $this->beginEach . '<label for="' . $name . '">' . $echo . '</label>' . $this->betweenEach . "<input type=\"$type\" name=\"$name\" id=\"$name\" $args />" . $this->endEach;
 		return $this;
 
 	}
 	public function addTextArea($name,$echo,$args='',$value=''){
-		$this->form.='<tr><td><label for="' . $name . '">' . $echo . '</label></td>'."<td><textarea name=\"$name\" id=\"$name\" $args>$value</textarea>";
+		$this->form.= $this->beginEach . '<label for="' . $name . '">' . $echo . '</label></td>' . $this->betweenEach . "<td><textarea name=\"$name\" id=\"$name\" $args>$value</textarea>" . $this->endEach;
 		return $this;
 	}
 	
@@ -24,11 +29,11 @@ class FormBuilder{
 	public function addSelectItemFromPdo($name,$echo,$query){
 	if(!$query)
 		throw new Exception('Query null in addSelectItemFromPdo');
-	$this->form.='<tr><td><label for="'.$name.'">'.$echo.'</label></td><td><select name="'.$name.'" id="'.$name.'">';
+	$this->form.= $this->beginEach . '<label for="' . $name . '">' . $echo . '</label>' . $this->betweenEach . '<select name="' . $name . '" id="' . $name . '">';
 	while($ligne=$query->fetch()){
 		$this->form.='<option value="'.$ligne[0].'">'.$ligne[1].'</option>';
 	}
-	$this->form.='</select></td>';
+	$this->form.='</select>'.$this->endEach;
 	return $this;
 }
 public function addHiddenInput($name,$value='1',$args=''){
@@ -38,10 +43,44 @@ public function addHiddenInput($name,$value='1',$args=''){
 
 
 	public function render(){
-		return $this->form.'</table></form>';
+		return $this->begin.$this->form.$this->end;
 	}
-	
+
+
+
+  /**
+   * @param string $begin
+   */
+  public function setBegin($begin) {
+    $this->begin = $begin;
+  }
+
+  /**
+   * @param string $end
+   */
+  public function setEnd($end) {
+    $this->end = $end;
+  }
+
+  /**
+   * @param string $beginEach
+   */
+  public function setBeginEach($beginEach) {
+    $this->beginEach = $beginEach;
+  }
+
+  /**
+   * @param string $betweenEach
+   */
+  public function setBetweenEach($betweenEach) {
+    $this->betweenEach = $betweenEach;
+  }
+
+  /**
+   * @param string $endEach
+   */
+  public function setEndEach($endEach) {
+    $this->endEach = $endEach;
+  }
 }
 
-
-?>
